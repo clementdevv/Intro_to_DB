00d13592e96922063ae487854cbe4a8ecad0e5f5
clementdevv <clementdevv@gmail.com>
2024-10-20 20:05:19 +0300

    added alx_book_store file

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Print the full description of the books table
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    COLUMN_KEY AS 'Key',
+    EXTRA AS 'Extra Info'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'Books';
